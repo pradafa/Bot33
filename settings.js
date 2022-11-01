4fe78f0bb2a838f662f2e433305ac0ad12c6c595
pradafa <114842660+pradafa@users.noreply.github.com>
2022-11-01 08:46:09 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "victor bot" //namabot kalian
-global.ownername= "leviXd" //nama kalian
+global.botname = "faa bot" //namabot kalian
+global.ownername= "DafaMV" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/channel/UC2GOH5mCArh3E4J3sGFMC5g" //bebas asal jan hapus
-global.myweb2 = "https://instagram.com/leviatnansyah?igshid=YmMyMTA2M2Y=" //bebas
-global.email = "leviadnansyahlevi@gmail.com" //bebas
+global.youtube = "https://youtube.com/channel/UC0O9GIULdyJJCX8x5akKfnw" //bebas asal jan hapus
+global.myweb2 = "https://www.instagram.com/mdfaa04" //bebas
+global.email = "okepradafa@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6285172446692'] //premium user
-global.owner = ["6285172446692","6285172446692"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6285172446692" // nomor wa kalian
-global.ownernomerr = "6285172446692" //nmr wa kalian
+global.premium = ['6289514653159'] //premium user
+global.owner = ["6289514653159","6289514653159"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6289514653159" // nomor wa kalian
+global.ownernomerr = "6289514653159" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© victor bot' //sticker wm ubah
-global.author = 'Di Buat Oleh LeviXd' //sticker wm ganti nama kalian
+global.packname = '© faa bot' //sticker wm ubah
+global.author = 'Di Buat Oleh DafaMV' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
@@ -125,7 +125,7 @@ global.mess = {
     private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
 	bot: 'Fitur Khusus Pengguna Nomor Bot',
     errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
+    wait: '⏳ Sabar Mek',
 	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
 	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
